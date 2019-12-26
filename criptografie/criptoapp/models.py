45e6a66dcd25347ef28469b1dcc97dea6d2d3a36
peteralexy <pete_1124@yahoo.com>
2019-12-26 13:05:03 +0200

    form and submit button

--- criptografie/criptoapp/models.py
+++ criptografie/criptoapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Voter(models.Model):
+    voter_name = models.CharField(max_length=50)
+    voter_age = models.IntegerField(null=True)
+    voter_city = models.CharField(max_length=50)
+    voter_county = models.CharField(max_length=50)
+    voter_address = models.CharField(max_length=100)
+    voter_vote = models.CharField(max_length=50)
