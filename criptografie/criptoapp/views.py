32b7bbd1a71d2761b41600cc416760d22d80bf84
peteralexy <pete_1124@yahoo.com>
2019-12-26 14:37:23 +0200

    added a mysql connection

--- criptografie/criptoapp/views.py
+++ criptografie/criptoapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from .forms import VoteForm
+import mysql.connector as mc
 
 # Create your views here.
 def index(request):
@@ -14,7 +15,10 @@ def page(request):
     else:
         form = VoteForm(request.POST)
         if form.is_valid():
-            form.save()
+            #form.save()
+            #open a connection to mysql workbench
+            #mySQLconnection = mc.connect()
+
             return HttpResponseRedirect(reverse('criptoapp:index'))
 
     return render(request, 'page.html', {'form': form})
