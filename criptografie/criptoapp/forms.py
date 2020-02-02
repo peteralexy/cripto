ebedb4bbd210d3655f0e0239d9fef63f82ec7a3c
sabo_ <sabo_cosmin_ioan@yahoo.com>
2020-02-02 18:24:33 +0200

    ce vrei tu

--- criptografie/criptoapp/forms.py
+++ criptografie/criptoapp/forms.py
@@ -4,4 +4,4 @@ from .models import Voter
 class VoteForm(ModelForm):
     class Meta:
         model = Voter
-        fields = ['voter_vote']
+        fields = ['voter_name', 'voter_age', 'voter_city', 'voter_county', 'voter_address', 'voter_vote']
--- criptografie/criptoapp/views.py
+++ criptografie/criptoapp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from .forms import VoteForm
+from .models import Voter
+from .RSA import *
+import mysql.connector as mc
 
 # Create your views here.
 def index(request):
@@ -14,7 +17,58 @@ def page(request):
     else:
         form = VoteForm(request.POST)
         if form.is_valid():
-            form.save()
+      
+            voter = Voter()
+            voter.voter_name = form.data['voter_name']
+            voter.voter_age = form.data['voter_age']
+            voter.voter_city = form.data['voter_city']
+            voter.voter_county = form.data['voter_county']
+            voter.voter_address = form.data['voter_address']
+            voter.voter_vote = form.data['voter_vote']
+
+            p = 331
+            q = 137
+
+            public, private = generate_keypair(p, q)
+            message = str(voter.voter_vote)
+            encrypted_msg = encrypt(private, message)
+            decrypted_msg = decrypt(public, encrypted_msg)
+            decrypted_msg
+            #conn to mysq;
+            
+            mysqslcc = mc.connect(host='localhost', user='root', password='1234', database='evoting')
+
+            cursor = mysqslcc.cursor()
+
+            cursor.execute(
+                """
+                SELECT * FROM evoting.voter
+                WHERE name LIKE '%"""+ voter.voter_name +"""%' AND age LIKE '%""" + voter.voter_age+ """%' AND city LIKE '%""" + voter.voter_city+ """%' AND country LIKE '%""" + voter.voter_county+ """%' AND address LIKE '%""" + voter.voter_address+ """%'; 
+                """
+            )
+
+            c = cursor.fetchall()
+            
+            if len(c) == 0:
+                cursor.execute(
+                    """
+                    INSERT INTO evoting.voter
+                    VALUES
+                    ('"""+ voter.voter_name +"""', '""" + voter.voter_age+ """', '""" + voter.voter_city+ """', '""" + voter.voter_county+ """', '""" + voter.voter_address+ """');
+                    """
+                    
+                )
+            
+                cursor.execute(
+                    """
+                    UPDATE evoting.candidate
+                    SET votes = votes + 1
+                    WHERE name LIKE '%"""+ decrypted_msg +"""%';
+                    """
+                    
+                )
+                mysqslcc.commit()
+            
             return HttpResponseRedirect(reverse('criptoapp:index'))
 
     return render(request, 'page.html', {'form': form})
